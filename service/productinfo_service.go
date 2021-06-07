0adf851ea80d48827532fca08b7720d413d46885
Dmitry Ovchinnikov <d.ovchinnikov@greatbook.ru>
2021-06-07 18:37:29 +0300

    Added general structure of the project. Made pb file and server.

---
+++ service/productinfo_service.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	pb "productinfo/service/ecommerce"
+)
+
+// Server used to for realisation ecommerce/product_info
+type server struct {
+	productMap map[string]*pb.Product
+}
+
+// AddProduct realise ecommerce.AddProduct
+func (s *server) AddProduct(ctx context.Context, in *pb.Product) (*pb.ProductID, error) {
+	out, err := uuid.NewV4()
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Error while generating Product ID", err)
+	}
+	in.Id = out.String()
+	if s.productMap == nil {
+		s.productMap = make(map[string]*pb.Product)
+	}
+	s.productMap[in.Id] = in
+
+	return &pb.ProductID{Value: in.Id}, status.New(codes.OK, "").Err()
+}
+
+// GetProduct realise ecommerce.GetProduct
+func (s *server) GetProduct(ctx context.Context, in *pb.ProductID) (*pb.Product, error) {
+	value, exists := s.productMap[in.Value]
+	if exists {
+		return value, status.New(codes.OK, "").Err()
+	}
+
+	return nil, status.Errorf(codes.NotFound, "Product does not exist.", in.Value)
+}
+
