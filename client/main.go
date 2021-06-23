3c3edc9033cb7a39f5bc7c32af53a0992b12324a
Dmitry Ovchinnikov <d.ovchinnikov@greatbook.ru>
2021-06-23 17:29:08 +0300

    Server- and client- side OrderManagement added. Get Order works fine.

--- client/main.go
+++ client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"github.com/golang/protobuf/ptypes/wrappers"
 	"google.golang.org/grpc"
 	"log"
 	pb "productinfo/client/ecommerce"
@@ -13,34 +14,56 @@ const (
 )
 
 func main() {
+	// Setting up a connection to the server.
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	c := pb.NewProductInfoClient(conn)
-
-	name := "Apple iPhone 11"
-	description := `Meet Apple iPhone 11. All-new dual-camera system with Ultra Wide and Night Mode.`
-	price := float32(1000.0)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	//c := pb.NewProductInfoClient(conn)
+	c := pb.NewOrderManagementClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
 
-	r, err := c.AddProduct(ctx, &pb.Product{
-		Name: name,
-		Description: description,
-		Price: price,
-			})
-	if err != nil {
-		log.Fatalf("Could not add product: %v", err)
+	// Add Order
+	order1 := pb.Order{
+		Id: "101",
+		Items:	[]string{"iPhone XS", "MacBook Pro"},
+		Price: 2300.00,
+		Description: "San Jose, CA",
 	}
-	log.Printf("Product ID: %s added successfully", r.Value)
+	res, _ := c.AddOrder(ctx, &order1)
+	if res != nil {
+		log.Print("AddOrder Response -> ", res.Value)
+	}
 
-	product, err := c.GetProduct(ctx, &pb.ProductID{
-		Value: r.Value,
-	})
-	if err != nil {
-		log.Fatalf("Could not get product: %v", err)
-	}
-	log.Printf("Product: %v", product.String())
+	// Get Order
+	retrievedOrder, err := c.GetOrder(ctx, &wrappers.StringValue{Value: "106"})
+	log.Print("GetOrder Response -> : ", retrievedOrder)
+
+	// // Add Product
+	//name := "Apple iPhone 11"
+	//description := `Meet Apple iPhone 11. All-new dual-camera system with Ultra Wide and Night Mode.`
+	//price := float32(1000.0)
+	//ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	//defer cancel()
+	//
+	//r, err := c.AddProduct(ctx, &pb.Product{
+	//	Name: name,
+	//	Description: description,
+	//	Price: price,
+	//		})
+	//if err != nil {
+	//	log.Fatalf("Could not add product: %v", err)
+	//}
+	//log.Printf("Product ID: %s added successfully", r.Value)
+
+	// // Get Product
+	//product, err := c.GetProduct(ctx, &pb.ProductID{
+	//	Value: r.Value,
+	//})
+	//if err != nil {
+	//	log.Fatalf("Could not get product: %v", err)
+	//}
+	//log.Printf("Product: %v", product.String())
 }
--- service/main.go
+++ service/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"github.com/gofrs/uuid"
+	"github.com/golang/protobuf/ptypes/wrappers"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"log"
 	"net"
 	pb "productinfo/service/ecommerce"
@@ -11,16 +16,73 @@ const (
 	port = ":50051"
 )
 
+var orderMap = make(map[string]pb.Order)
+
+// server is used to implement ecommerce/product_info.
+type server struct {
+	productMap map[string]*pb.Product
+	orderMap map[string]*pb.Order
+}
+
+// AddProduct implements ecommerce.AddProduct
+func (s *server) AddProduct(ctx context.Context, in *pb.Product) (*pb.ProductID, error) {
+	out, err := uuid.NewV4()
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Error while generating Product ID", err)
+	}
+	in.Id = out.String()
+	if s.productMap == nil {
+		s.productMap = make(map[string]*pb.Product)
+	}
+	s.productMap[in.Id] = in
+
+	return &pb.ProductID{Value: in.Id}, status.New(codes.OK, "").Err()
+}
+
+// GetProduct implements ecommerce.GetProduct
+func (s *server) GetProduct(ctx context.Context, in *pb.ProductID) (*pb.Product, error) {
+	value, exists := s.productMap[in.Value]
+	if exists {
+		return value, status.New(codes.OK, "").Err()
+	}
+
+	return nil, status.Errorf(codes.NotFound, "Product does not exist.", in.Value)
+}
+
+// GetOrder service implementation.
+func (s *server) GetOrder(ctx context.Context, orderId *wrappers.StringValue) (*pb.Order, error) {
+
+	// Service Implementation
+	ord, exist := orderMap[orderId.Value]
+	if !exist {
+		return nil, status.Errorf(codes.NotFound, "Order does not exist. Order ID: %s", orderId)
+	}
+
+	return &ord, status.New(codes.OK, "").Err()
+}
+
 func main() {
+	initSampleData()
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterProductInfoServer(s, &server{})
+	//pb.RegisterProductInfoServer(s, &server{})
+	pb.RegisterOrderManagementServer(s, &server{})
 
 	log.Println("Starting gRPC listener on port " + port)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
+
+func initSampleData() {
+	orderMap["102"] = pb.Order{Id: "102", Items: []string{"Google Pixel 3A", "Mac Book Pro"}, Destination: "Mountain View, CA", Price: 1800.00}
+	orderMap["103"] = pb.Order{Id: "103", Items: []string{"Apple Watch S4"}, Destination: "San Jose, CA", Price: 400.00}
+	orderMap["104"] = pb.Order{Id: "104", Items: []string{"Google Home Mini", "Google Nest Hub"}, Destination: "Mountain View, CA", Price: 400.00}
+	orderMap["105"] = pb.Order{Id: "105", Items: []string{"Amazon Echo"}, Destination: "San Jose, CA", Price: 30.00}
+	orderMap["106"] = pb.Order{Id: "106", Items: []string{"Amazon Echo", "Apple iPhone XS"}, Destination: "Mountain View, CA", Price: 300.00}
+}
+
